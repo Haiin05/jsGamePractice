f7b3f6816d8afd5c2716cd7c1ceacef7a6ec030f
Haiin <h986680@gmail.com>
2020-10-23 22:37:14 +0900

    game-css

--- bg.js
+++ bg.js
@@ -1,10 +1,8 @@
 const body = document.querySelector("body");
 
-
 const imageNumber = Math.floor(Math.random() * 3); // 랜덤숫자 뽑아주기 0~2
 
 const images = new Image(); // new Image() 는 이미지 객체 생성해주는 함수
 body.prepend(images);
 images.src = `image/image0${imageNumber + 1}.jpg`;
 images.classList.add("bgimage");
-
